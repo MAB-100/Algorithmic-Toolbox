1d29284ac5181b4a9864f047375522df85b43541
MAB-100 <shahabbas661@gmail.com>
2024-08-05 18:53:48 +0500

    week 2 problem 3

--- LABS/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ LABS/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -1,12 +1,9 @@
 def gcd(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
-
-    return current_gcd
-
+    if b == 0:
+        return a
+    else:
+        c = a % b
+        return gcd(b, c)    
 
 if __name__ == "__main__":
     a, b = map(int, input().split())
