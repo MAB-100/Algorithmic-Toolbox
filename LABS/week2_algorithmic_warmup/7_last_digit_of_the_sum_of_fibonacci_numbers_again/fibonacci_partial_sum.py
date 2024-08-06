2872f552e25db0dfcccdc40038ef7abb8c5092fc
MAB-100 <shahabbas661@gmail.com>
2024-08-06 12:24:54 +0500

    week 2 problem 8

--- LABS/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ LABS/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -1,22 +1,34 @@
-# Uses python3
-import sys
 
-def fibonacci_partial_sum_naive(from_, to):
-    _sum = 0
+def pisano_period(m):
+    a, b = 0, 1
+    for i in range(0, m*m):
+        a, b = b, (a+b + 1) % m
+        if a == 0 and b == 1:
+            return i + 1
 
-    current = 0
-    _next  = 1
+def fibonacci_number(n):
+    list = []
+    list.append(0)
+    list.append(1)
+    for i in range(2, n+1):
+        list.append(list[i-1] + list[i-2] + 1)
+    return list[n]
 
-    for i in range(to + 1):
-        if i >= from_:
-            _sum += current
 
-        current, _next = _next, current + _next
+def fibonacci_partial_sum(n, m):
+    if n < 0:
+        return 0
+    pissan_period = pisano_period(m)
+    n = n % pissan_period
+    return fibonacci_number(n) % m
 
-    return _sum % 10
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
-    from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    from_, to = map(int, input().split())
+
+    print((fibonacci_partial_sum(to, 10) - fibonacci_partial_sum(from_ - 1 , 10)) % 10)
+    
+    # while True:
+    #     x = int(input())
+    #     print(fibonacci_binet(x))
--- LABS/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ LABS/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -1,16 +1,26 @@
-def fibonacci_sum_squares(n):
-    if n <= 1:
-        return n
+def pisano_period(m):
+    a, b = 0, 1
+    for i in range(0, m*m):
+        a, b = b, (a+b) % m
+        if a == 0 and b == 1:
+            return i + 1
 
-    previous, current, sum = 0, 1, 1
+def fibonacci_number(n):
+    list = []
+    list.append(0)
+    list.append(1)
+    for i in range(2, n+1):
+        list.append(list[i-1] + list[i-2])
+    return list[n]
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current * current
 
-    return sum % 10
+def fibonacci_huge_naive(n, m):
+    pissan_period = pisano_period(m)
+    n = n % pissan_period
+    return fibonacci_number(n) % m
 
 
 if __name__ == '__main__':
     n = int(input())
-    print(fibonacci_sum_squares(n))
+    print((fibonacci_huge_naive(n, 10) * fibonacci_huge_naive(n+1, 10)) % 10)
+
