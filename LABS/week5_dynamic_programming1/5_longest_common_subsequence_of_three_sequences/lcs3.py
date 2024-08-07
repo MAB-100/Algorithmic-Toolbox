7fc205f50e98617f677e98377a1e5d85d8b34228
MAB-100 <shahabbas661@gmail.com>
2024-08-07 12:13:01 +0500

    week 5 problem 5

--- LABS/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
+++ LABS/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.py
@@ -1,5 +1,22 @@
 def lcs3(first_sequence, second_sequence, third_sequence):
-    return 0
+    n = len(first_sequence)
+    m = len(second_sequence)
+    q = len(third_sequence)
+    
+    # Initialize the DP table
+    dp = [[[0] * (q + 1) for _ in range(m + 1)] for _ in range(n + 1)]
+    
+    # Fill the DP table
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            for k in range(1, q + 1):
+                if first_sequence[i - 1] == second_sequence[j - 1] == third_sequence[k - 1]:
+                    dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1
+                else:
+                    dp[i][j][k] = max(dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1])
+    
+    # Return the length of the longest common subsequence
+    return dp[n][m][q]
 
 if __name__ == '__main__':
     n = int(input())
