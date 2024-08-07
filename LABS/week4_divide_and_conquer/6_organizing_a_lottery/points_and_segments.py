ec201cb017401ee4a7a6df56622ef6cccf5dbf02
MAB-100 <shahabbas661@gmail.com>
2024-08-07 10:30:38 +0500

    week 4 problem 6 wrong

--- LABS/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
+++ LABS/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.py
@@ -1,23 +1,40 @@
-from sys import stdin
-
-
-def points_cover_naive(starts, ends, points):
-    assert len(starts) == len(ends)
-    count = [0] * len(points)
-
-    for index, point in enumerate(points):
-        for start, end in zip(starts, ends):
-            if start <= point <= end:
-                count[index] += 1
-
-    return count
-
+def points_cover_optimized(starts, ends, points):
+    events = []
+    
+    # Create events for each segment start and end
+    for start, end in zip(starts, ends):
+        events.append((start, 'L'))  # Segment start
+        events.append((end + 1, 'R'))  # Segment end (exclusive)
+    
+    # Create events for each point query
+    for point in points:
+        events.append((point, 'P'))
+    
+    # Sort events: firstly by position, then by type ('L' -> 'P' -> 'R')
+    events.sort(key=lambda x: (x[0], x[1]))
+    
+    active_segments = 0
+    result = {}
+    
+    # Process events
+    for pos, typ in events:
+        if typ == 'L':
+            active_segments += 1
+        elif typ == 'R':
+            active_segments -= 1
+        elif typ == 'P':
+            result[pos] = active_segments
+    
+    # Collect results in the order of original points
+    return [result.get(point, 0) for point in points]
 
 if __name__ == '__main__':
+    from sys import stdin
     data = list(map(int, stdin.read().split()))
     n, m = data[0], data[1]
-    input_starts, input_ends = data[2:2 * n + 2:2], data[3:2 * n + 2:2]
-    input_points = data[2 * n + 2:]
-
-    output_count = points_cover_naive(input_starts, input_ends, input_points)
+    input_starts = data[2:2 + n]
+    input_ends = data[2 + n:2 + 2 * n]
+    input_points = data[2 + 2 * n:]
+    
+    output_count = points_cover_optimized(input_starts, input_ends, input_points)
     print(*output_count)
