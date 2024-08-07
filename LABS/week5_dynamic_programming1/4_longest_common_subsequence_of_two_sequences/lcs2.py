f0f313a409c7aa29f445c6fcdf057464cf0b40c7
MAB-100 <shahabbas661@gmail.com>
2024-08-07 10:44:53 +0500

    week 5 problem 4

--- LABS/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ LABS/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,6 +1,20 @@
 def lcs2(first_sequence, second_sequence):
-    return 0
-
+    n = len(first_sequence)
+    m = len(second_sequence)
+    
+    # Initialize a (n+1) x (m+1) DP table with 0
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    
+    # Fill the DP table
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if first_sequence[i - 1] == second_sequence[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    
+    # The length of the longest common subsequence
+    return dp[n][m]
 
 if __name__ == '__main__':
     n = int(input())
